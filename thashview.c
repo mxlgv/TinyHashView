29d275a9b9006714a81d0c690cf5e9d5af24c7e6
turbocat2001 <maximlogaev2001ezro@gmail.com>
2020-08-18 00:12:36 +0300

    add support for clipboard

--- thashview.c
+++ thashview.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "kos32sys1.h"
+#include "apis/kos32sys.h"
+#include "apis/errorbox.c"
 #include <string.h>
 #include <stdarg.h>
 #include "algorithms/md5.h"
@@ -8,22 +9,25 @@
 #include "algorithms/sha256.h"
 
 
-
+#define GREEN 0x00067D06
 #define MAX_HASH_LEN 128
 
 struct kolibri_system_colors sys_color_table;
 
-char hash_str_md5[MAX_HASH_LEN];
-char hash_str_sha1[MAX_HASH_LEN];
-char hash_str_sha256[MAX_HASH_LEN];
+char hash_str_md5[MAX_HASH_LEN]=   "Click the 'MD5:' button to show the md5-checksum!      ";
+char hash_str_sha1[MAX_HASH_LEN]=  "Click the 'SHA1:' button to show the sha1-checksum!    ";
+char hash_str_sha256[MAX_HASH_LEN]="Click the 'SHA256:' button to show the sha256-checksum!";
 char filename[FILENAME_MAX];
 char title[FILENAME_MAX+10];
 enum BUTTONS
 {
     BTN_QUIT=1,
-    BTN_MD5 = 16,
-    BTN_SHA1 = 32,
-    BTN_SHA256 = 64
+    BTN_MD5 = 10,
+    BTN_SHA1 = 20,
+    BTN_SHA256 = 30,
+    BTN_COPY_MD5= 11,
+    BTN_COPY_SHA1= 21,
+    BTN_COPY_SHA256=31
 };
 
 void md5_hash(FILE* input, BYTE* hash )
@@ -92,12 +96,11 @@ BYTE* check_sum(char *in_file_name, int alg)
     return hash;
 }
 
-void sprint_hash(BYTE *hash, char* hash_str)
+void sprint_hash(BYTE *hash, char* hash_str, int hash_size)
 {
     char block[2];
     char *temp_str;
     temp_str=malloc(MAX_HASH_LEN);
-    int hash_size = strlen((char*)hash);
     for(int i=0; i<hash_size; i++)
     {
         sprintf(block,"%02x", hash[i]);
@@ -111,21 +114,47 @@ void sprint_hash(BYTE *hash, char* hash_str)
 
 void redraw_window()
 {
-    sprintf(title,"%s - checksum", filename);
+    sprintf(title,"%s - thashview", filename);
     begin_draw();
-    sys_create_window(10, 40, 500, 120, title, 0xFFFFFF, 0x13);
-    define_button((10<<16)+80,(30<<16)+20, BTN_MD5, sys_color_table.work_button);
-    define_button((10<<16)+80,(60<<16)+20, BTN_SHA1, sys_color_table.work_button);
-    define_button((10<<16)+80,(90<<16)+20, BTN_SHA256, sys_color_table.work_button);
+    sys_create_window(50, 50, 665, 120, title, 0xFFFFFFFF, 0x14);
 
-    draw_text_sys("MD5:", 15, 34, 0, 0x90000000 | sys_color_table.work_button_text);
-    draw_text_sys("SHA1:", 15, 64, 0, 0x90000000 | sys_color_table.work_button_text);
+    define_button((10 << 16) + 60, (30 << 16) + 20, BTN_MD5, GREEN);
+    define_button((10 << 16) + 60, (60 << 16) + 20, BTN_SHA1, GREEN);
+    define_button((10 << 16) + 60, (90 << 16) + 20, BTN_SHA256, GREEN);
+
+    draw_text_sys("MD5:", 15, 34, 0,   0x90000000 | sys_color_table.work_button_text);
+    draw_text_sys("SHA1:", 15, 64, 0,  0x90000000 | sys_color_table.work_button_text);
     draw_text_sys("SHA256:", 15,94, 0, 0x90000000 | sys_color_table.work_button_text);
 
-    draw_text_sys(hash_str_md5, 100, 34, 0, 0x80000000 | sys_color_table.work_text);
-    draw_text_sys(hash_str_sha1, 100, 64, 0, 0x80000000 | sys_color_table.work_text);
-    draw_text_sys(hash_str_sha256, 100, 94, 0, 0x80000000| sys_color_table.work_text);
+    draw_text_sys(hash_str_md5, 80, 34, 0, 0x90000000 | sys_color_table.work_text);
+    draw_text_sys(hash_str_sha1, 80, 64, 0, 0x90000000 | sys_color_table.work_text);
+    draw_text_sys(hash_str_sha256, 80, 94, 0, 0x90000000| sys_color_table.work_text);
+
+    define_button((610 << 16) + 42, (30 << 16) + 20, BTN_COPY_MD5, sys_color_table.work_button);
+    define_button((610<< 16) + 42, (60 << 16) + 20, BTN_COPY_SHA1, sys_color_table.work_button);
+    define_button((610<< 16) + 42, (90 << 16) + 20, BTN_COPY_SHA256, sys_color_table.work_button);
+    draw_text_sys("copy", 615, 34, 0,   0x90000000 | sys_color_table.work_button_text);
+    draw_text_sys("copy", 615, 64, 0,  0x90000000 | sys_color_table.work_button_text);
+    draw_text_sys("copy", 615, 94, 0, 0x90000000 | sys_color_table.work_button_text);
     end_draw();
+}
+
+void copy_to_buffer(char *text)
+{
+    if(55!=strlen(text))
+    {
+    char temp_buffer[MAX_HASH_LEN];
+    *(temp_buffer+4)=0;
+    *(temp_buffer+8)=1;
+    strcpy(temp_buffer+12, text);
+    kol_clip_set(strlen(text)+12, temp_buffer);
+    }
+}
+
+void print_pending_calc(char *line)
+{
+  strcpy(line, "Please wait! Calculating checksum...                   ");
+  redraw_window();
 }
 
 int main(int argc, char** argv)
@@ -133,8 +162,15 @@ int main(int argc, char** argv)
     strcpy(filename, argv[1]);
     if(argc<2)
     {
+        error_box("No file selected!");
         exit(0);
     }
+    if(NULL==fopen(argv[1],"rb"))
+    {
+        error_box("File not found!");
+        exit(0);
+    }
+
     int gui_event;
     uint32_t pressed_button = 0;
     get_system_colors(&sys_color_table);
@@ -156,18 +192,36 @@ int main(int argc, char** argv)
             switch (pressed_button)
             {
                 case BTN_MD5:
-                    sprint_hash(check_sum(filename, MD5_BLOCK_SIZE),hash_str_md5);
+                    print_pending_calc(hash_str_md5);
+                    sprint_hash(check_sum(filename, MD5_BLOCK_SIZE),hash_str_md5, MD5_BLOCK_SIZE);
                     redraw_window();
                 break;
 
                 case BTN_SHA1:
-                    sprint_hash(check_sum(filename, SHA1_BLOCK_SIZE),hash_str_sha1);
+                    print_pending_calc(hash_str_sha1);
+                    sprint_hash(check_sum(filename, SHA1_BLOCK_SIZE),hash_str_sha1, SHA1_BLOCK_SIZE);
                     redraw_window();
                 break;
 
                 case BTN_SHA256:
-                    sprint_hash(check_sum(filename, SHA256_BLOCK_SIZE),hash_str_sha256);
+                    print_pending_calc(hash_str_sha256);
+                    sprint_hash(check_sum(filename, SHA256_BLOCK_SIZE),hash_str_sha256, SHA256_BLOCK_SIZE);
                     redraw_window();
+                break;
+
+                case BTN_COPY_MD5:
+                     redraw_window();
+                     copy_to_buffer(hash_str_md5);
+                break;
+
+                case BTN_COPY_SHA1:
+                     redraw_window();
+                     copy_to_buffer(hash_str_sha1);
+                break;
+
+                case BTN_COPY_SHA256:
+                     redraw_window();
+                     copy_to_buffer(hash_str_sha256);
                 break;
 
                 case BTN_QUIT:
@@ -176,6 +230,7 @@ int main(int argc, char** argv)
             }
         }
     }while(1);
+
 }
 
 
