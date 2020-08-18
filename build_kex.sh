a7af1b8a150c948d66725009779b9eda23772780
turbocat2001 <maximlogaev2001ezro@gmail.com>
2020-08-18 19:44:11 +0300

    add libmsgbox prototype

--- build_kex.sh
+++ build_kex.sh
@@ -1,2 +1,2 @@
 #SHS
-/kolibrios/develop/tcc/tcc algorithms/md5.c algorithms/sha1.c algorithms/sha256.c thashview.c -o thashview -lck
+/kolibrios/develop/tcc/tcc algorithms/md5.c algorithms/sha1.c apis/msgbox.c algorithms/sha256.c thashview.c -o thashview -lck
