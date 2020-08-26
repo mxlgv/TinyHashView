16e3d33ee2278d3f993ae4a32e57a7bdcb6a3549
turbocat2001 <maximlogaev2001ezro@gmail.com>
2020-08-27 00:08:48 +0300

    delete debug arguments

--- build_kex.sh
+++ build_kex.sh
@@ -1,3 +1,3 @@
 #SHS
-/kolibrios/develop/tcc/tcc algorithms/md5.c algorithms/sha1.c libmsgbox/msgbox.c algorithms/sha256.c thashview.c -o thashview -lck -g
+/kolibrios/develop/tcc/tcc algorithms/md5.c algorithms/sha1.c algorithms/sha256.c thashview.c -o thashview -lck
 thashview LICENSE
