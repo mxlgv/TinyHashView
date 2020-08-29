71360f182134657cf34f48e712d37f894c66a4ad
gimadutdin <33692565+gimadutdin@users.noreply.github.com>
2020-08-30 01:01:44 +0300

    in kos32sys.h in start_app added pragma's for correct packing struct when using mingw

--- kos32sys.h
+++ kos32sys.h
@@ -695,6 +695,7 @@ void shm_close(char *shm_name){
 
 static inline
 int start_app(char *app_name, char *args){
+    #pragma pack(push, 1)
     struct file_op_t
     {
         uint32_t    fn;
@@ -704,6 +705,7 @@ int start_app(char *app_name, char *args){
         char        zero;
         char*       app_name  __attribute__((packed));
     } file_op;
+    #pragma pack(pop)
     memset(&file_op, 0, sizeof(file_op));
     file_op.fn = 7;
     file_op.args = args;
